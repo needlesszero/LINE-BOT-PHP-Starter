8b25bf9543ae8a7a8d5c5a977d1b2b0d73e9685c
Blue Applez <blue.applez04@gmail.com>
2018-01-09 13:57:23 +0700

    aa

--- bot.php
+++ bot.php
@@ -34,6 +34,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => 'link status :'.$obj[$link]
 			];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
