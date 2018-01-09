90b32e79cf65af0c801f21cd0248904cf8e62c3c
Blue Applez <blue.applez04@gmail.com>
2018-01-09 16:56:55 +0700

    ฟหก

--- bot.php
+++ bot.php
@@ -30,7 +30,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $status
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
