afaf0a16a657e448296f327cd5449fdedd62e68a
Blue Applez <blue.applez04@gmail.com>
2018-01-09 11:00:36 +0700

    v3

--- bot.php
+++ bot.php
@@ -12,7 +12,12 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
+			if($event['message']['text'] == '55555'){
+				$text = 'หัวเราะอะไร'
+			}
+			else {
 			$text = $event['message']['text'];
+			}
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
