b10a3e938f290a792f5f991c9da919638e02f219
Blue Applez <blue.applez04@gmail.com>
2018-01-10 10:07:39 +0700

    ๅ/-

--- bot.php
+++ bot.php
@@ -1,18 +1,19 @@
 <?php
+
+$data = '{
+	"name": "Aragorn",
+	"race": "Human"
+}';
+
+$character = json_decode($data);
+echo $character->name;
+
 $access_token = 'uqhIt5rrQz+lIsYeKXJj1vN0uN/iGOI82cpggWhyLW/0xqEAudj9NCX+YgM9HA+lOROLnYrlXO5peau/5MeriEs/kUu4iu0WojXBWLqXqj7QDByc60mpmBvWUGg/gfSoxyRNlv4BwVwdD+wsgmpzDgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-
-
-$json = file_get_contents('https://powerful-badlands-66623.herokuapp.com/test.json');
-$jsons = json_decode($content, true);
-
-// Validate parsed JSON data
-
-echo $content;
 
 if (!is_null($events['events'])) {
 	// Loop through each event
@@ -52,4 +53,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OKttt";
+echo "OK";
