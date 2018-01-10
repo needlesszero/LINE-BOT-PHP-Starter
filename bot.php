dd6760970d25bc449b5f3d875c988246c1bf060e
Blue Applez <blue.applez04@gmail.com>
2018-01-10 09:29:24 +0700

    231

--- bot.php
+++ bot.php
@@ -6,17 +6,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 
-
-$json = file_get_contents('https://powerful-badlands-66623.herokuapp.com/test.json');
-$jsons = json_decode($content, true);
-
-// Validate parsed JSON data
-
-echo $events;
-
-foreach ($jsons['events'] as $jso) {
-		$status =  $jso['type'];
-	}
+echo $events ;
 
 if (!is_null($events['events'])) {
 
