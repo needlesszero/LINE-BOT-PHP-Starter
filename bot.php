fc6d848a91d26044d9b8b8858ad8a6b2fb7d48c1
Blue Applez <blue.applez04@gmail.com>
2018-01-10 11:48:32 +0700

    456

--- bot.php
+++ bot.php
@@ -16,23 +16,20 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 
-$result = file_get_contents($url);
-var_dump(json_decode($result, true));
-
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$ = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $t
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
