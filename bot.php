31290984aa9d4edfcf344d5a217080672bf86c3a
Blue Applez <blue.applez04@gmail.com>
2018-01-11 14:31:32 +0700

    123

--- bot.php
+++ bot.php
@@ -11,7 +11,7 @@ $content = file_get_contents($url);
 $json = json_decode($content, true);
 
 echo $json;
-echo $json['results'];
+echo $json['results'][1];
 
 if (!is_null($events['events'])) {
 	// Loop through each event
