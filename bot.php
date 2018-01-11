07fb761da2bf96e46d57decc4a7fbba40860a28a
Blue Applez <blue.applez04@gmail.com>
2018-01-11 15:30:34 +0700

    123

--- bot.php
+++ bot.php
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) {
 				else 
 					foreach ($js['address_components'] as $key => $value) {
 						if($event['message']['text'] == 'status'){
-							$tt = $key;
+							$tt = $key['long_name'];
 							break;
 						}
 						else $tt = 'fails';
