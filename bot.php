e28fd5e0136857924c760338e1bcd5e4aca37dfc
Blue Applez <blue.applez04@gmail.com>
2018-01-11 13:42:23 +0700

    ๅ/-ๅ/-

--- bot.php
+++ bot.php
@@ -11,7 +11,7 @@ $content = file_get_contents($url);
 $json = json_decode($content, true);
 
 echo $json;
-echo $json['results']['address_components']['long_name'];
+echo $json['results']['address_components'];
 
 
 if (!is_null($events['events'])) {
