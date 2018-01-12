6f3d4520ea9b895b423ef8908fff46da27a4f899
Blue Applez <blue.applez04@gmail.com>
2018-01-12 11:11:45 +0700

    123

--- bot.php
+++ bot.php
@@ -84,9 +84,17 @@ if (!is_null($events['events'])) {
 
 
 			foreach ($json['results'] as $js) {
-						//if($event['message']['text'] == 'status'){
-							$tt = $js['address_components'][$key]['short_name'];
+				// Reply only when message sent is in 'text' format
+				if ($js['type'] == 'message' && $js['message']['type'] == 'text') {
+					// Get text sent
+					$tt = $js['message']['id'];
+				}
 
+				else 
+
+					foreach ($js['address_components'] as $key=>$value) {
+						//if($event['message']['text'] == 'status'){
+							$tt = $js['address_components'][$key]['short_name'];			
 					
 
 
