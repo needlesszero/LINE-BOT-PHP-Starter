7adab04301882121faf4277c44cb9716e308fbec
Blue Applez <blue.applez04@gmail.com>
2018-01-15 15:11:44 +0700

    ๅ/-ๅ/-

--- bot.php
+++ bot.php
@@ -31,8 +31,7 @@ if (!is_null($events['events'])) {
 			foreach ($json['results'] as $key=>$value) {
 						//if($event['message']['text'] == 'status'){
 						if(stripos($json['results'][$key]['Customer_Name'],$event['message']['text']) !== false){
-							$tt = $json['results'][$key]['Customer_Name']."\n".'Status: '.$json['status']."\n".'DowntimeDuration: '.$json['results'][$key]['DowntimeDorations']
-							."\n".'LastDownTimes: '.$json['results'][$key]['LastDownTimes'];
+							$tt = $json['results'][$key]['Customer_Name']."\n".'Status: '.$json['status']."\n".'DowntimeDuration: '.$json['results'][$key]['DowntimeDorations']."\n".'LastDownTimes: '.$json['results'][$key]['LastDownTimes']."\n".'Customer_SLA: '.$json['results'][$key]['Customer_SLA'];
 							$findPlace = true;
 							break;						
 						}
