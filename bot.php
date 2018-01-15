607be6cfa4cbb03e656e10373681352349b15df3
Blue Applez <blue.applez04@gmail.com>
2018-01-15 10:30:36 +0700

    ๅ/-

--- bot.php
+++ bot.php
@@ -1,25 +1,124 @@
-<?php // callback.php
-define("LINE_MESSAGING_API_CHANNEL_SECRET", '8a3bca6e2e763320804298a01559b90b');
-define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'xXHOO2/cmUFZ2DgFpz0P0beBcReaI5o25ZcFGa6VwP/miLf0Sw0XyQonjMdigSAXaq4UiDaAkgw6nhBAb8vALpu8lZstE3nmAEJm1FPO73g3uV8C3h8lyRkA0D7qQNlcmqjxeofB5+EUmwZ5voNWyQdB04t89/1O/w1cDnyilFU=');
+<?php
+$access_token = 'KygJBTnV/xAS9QNhJgQymbEZFw92G8Mj0RjrD3ycZEYbO9+I1a4e4dUqbvIo9Rv+OROLnYrlXO5peau/5MeriEs/kUu4iu0WojXBWLqXqj60DFs60UEbMhmV1fc5mEFF+GXDdqzqmAs+50FUkrVwCwdB04t89/1O/w1cDnyilFU=';
 
-require __DIR__."/../vendor/autoload.php";
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
 
-$bot = new \LINE\LINEBot(
-    new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
-    ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
-);
+$url = 'https://raw.githubusercontent.com/needlesszero/LINE-BOT-PHP-Starter/master/im.json';
+$content = file_get_contents($url);
+$json = json_decode($content, true);
 
-$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$body = file_get_contents("php://input");
+echo $json;
+echo $json['results']['address_components'][1]['long_name'];
 
-$events = $bot->parseEventRequest($body, $signature);
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
 
-foreach ($events as $event) {
-    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
-        $reply_token = $event->getReplyToken();
-        $text = $event->getText();
-        $bot->replyText($reply_token, $text);
-    }
+
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+
+
+			foreach ($json['results'] as $js) {
+				// Reply only when message sent is in 'text' format
+				if ($js['type'] == 'message' && $js['message']['type'] == 'text') {
+					// Get text sent
+					$tt = $js['message']['id'];
+				}
+
+				else 
+
+					foreach ($js['address_components'] as $key=>$value) {
+						//if($event['message']['text'] == 'status'){
+						if(stripos($js['address_components'][$key]['long_name'],$event['message']['text']) !== false){
+							$tt = $js['address_components'][$key]['short_name'];
+							break;						
+						}
+						else $tt = 'fails';
+					}
+					
+			}
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $tt
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+
+		//if sent by sticker
+		if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+
+
+			foreach ($json['results'] as $js) {
+
+			foreach ($js['address_components'] as $key=>$value) {
+						//if($event['message']['text'] == 'status'){
+				$tt = $js['address_components'][$key]['short_name'];			
+					
+
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $tt
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+						}
+		}
+	}
+	}
 }
-
 echo "OK";
