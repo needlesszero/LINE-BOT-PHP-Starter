fc186bb9902021be90b840e7745715c0ececd1bf
Blue Applez <blue.applez04@gmail.com>
2018-01-15 15:59:31 +0700

    234234

--- bot.php
+++ bot.php
@@ -60,8 +60,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				('type' => 'text',
-				'text' => $tt)
+				['type' => 'text',
+				'text' => $tt],
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
