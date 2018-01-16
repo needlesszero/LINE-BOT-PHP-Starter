fe91e7019b27e24a8a97657f4ffa505f528d1577
Blue Applez <blue.applez04@gmail.com>
2018-01-16 13:48:45 +0700

    123123

--- bot.php
+++ bot.php
@@ -78,10 +78,19 @@ if (!is_null($events['events'])) {
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
+
+			if($findPlace==false){	
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages[0]],
+			];
+		}
+		else {
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages[0],$messages[1]],
 			];
+		}
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
