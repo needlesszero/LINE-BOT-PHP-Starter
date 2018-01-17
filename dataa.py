6e397fd9a6894e48fdd377da3c9572afb4819bce
Blue Applez <blue.applez04@gmail.com>
2018-01-17 15:57:01 +0700

    123

--- dataa.py
+++ dataa.py
@@ -3,6 +3,15 @@ import timeit
 import requests
 import os
 start = timeit.default_timer()
+cNull = 0
+cSave = 0
+def set_Null():
+	global cNull    # Needed to modify global copy of globvar
+	cNull += 1
+def set_Save():
+	global cSave   # Needed to modify global copy of globvar
+	cSave += 1
+
 def get_bitcoin_returnop(_block_start,_block_end,_folder):
 	os.mkdir(_folder)
 	for i in range (_block_start,_block_end+1):
@@ -15,15 +24,23 @@ def get_bitcoin_returnop(_block_start,_block_end,_folder):
 		resp = json.loads(json_str) #load json
 		var = resp['timestamp'] #get data timestamp
 		if not var: #if json is null
+			set_Null()
 			print i," data empty, Not Save"
+			global cNull
+			
 		else :
+			set_Save()
 		#with urllib.request.urlopen(bitcoin_block_url) as url:
 			#data=json.loads(url.read().decode())
 			file_path = _folder+'/'+str(i)+'.json'
 			with open(file_path,'w')as f :
 				json.dump(data, f)
-				print(i)
-get_bitcoin_returnop(0,4000,'block_0_100')
+				print i," Save"
+				
+
+	
+get_bitcoin_returnop(349850,349900,'block_0_100')
 stop = timeit.default_timer()
+print ("Data : ",cSave," Data Block and ",cNull," Data Null")
 print ("RUN TIME !! : ",stop - start)
 
