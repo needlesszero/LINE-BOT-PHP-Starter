1d90af68925e428718ed02c4d288895f9450874d
needlesszero <blue.applez04@gmail.com>
2018-01-29 14:46:21 +0700

    ๅ/-/ๅ

--- bot.php
+++ bot.php
@@ -99,7 +99,7 @@ if (!is_null($events['events'])) {
 								$findPlace = true;
 								break;						
 							}
-							else $tt = 'ไม่พบข้อมูล';	
+							else $tt = 'ไม่พบข้อมูล';
 						}
 
 						elseif(preg_match('/^-ltd/', $event['message']['text']) || preg_match('/^-lastd/', $event['message']['text'])){
@@ -173,7 +173,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				['type' => 'text',
-				'text' => $tt],
+				'text' => 'Username-Authentication'],
 				['type' => 'text',
 				'text' => $tt]
 			];
@@ -196,20 +196,48 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 
-
+			if(stripos('needlesszero',$event['message']['text']){
 			$url = 'https://powerful-badlands-66623.herokuapp.com/im.json';
 			$content = file_get_contents($url);
-			if (!is_null($events['events'])) {
-	// Loop through each event
-				foreach ($events['events'] as $event) {
-					// Reply only when message sent is in 'text' format
-					if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-						// Get text sent
-						$text = $event['message']['text'];
-						// Get replyToken
-						$replyToken = $event['replyToken'];
+
+				if (!is_null($events['events'])) {
+					// Loop through each event
+					foreach ($events['events'] as $event) {
+						// Reply only when message sent is in 'text' format
+						if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+							// Get text sent
+							$text = $event['message']['text'];
+							// Get replyToken
+							$replyToken = $event['replyToken'];
+
+							$messages = [
+							['type' => 'text',
+							'text' => 'Username-Authentication'],
+							['type' => 'text',
+							'text' => $tt]
+							];
+
+			// Make a POST Request to Messaging API to reply to sender
+							$url = 'https://api.line.me/v2/bot/message/reply';
+							$data = [
+								'replyToken' => $replyToken,
+								'messages' => [$messages[1]],
+							];
+							$post = json_encode($data);
+							$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+							$ch = curl_init($url);
+							curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+							curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							$result = curl_exec($ch);
+							curl_close($ch);
+						}
 					}
 				}
+
 			}
 
 			echo $result . "\r\n";
