35e835c385ccdda2d2bf8e0a76623b55a4cf6ffa
needlesszero <blue.applez04@gmail.com>
2018-01-29 16:31:10 +0700

    ๅ/-

--- bot.php
+++ bot.php
@@ -8,6 +8,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 
+
 $url = 'https://powerful-badlands-66623.herokuapp.com/im.json';
 $content = file_get_contents($url);
 $json = json_decode($content, true);
@@ -173,7 +174,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				['type' => 'text',
-				'text' => $tt],
+				'text' => 'Username-Authentication'],
 				['type' => 'text',
 				'text' => $tt]
 			];
@@ -195,6 +196,47 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
+
+
+			$url = 'https://powerful-badlands-66623.herokuapp.com/im.json';
+			$content = file_get_contents($url);			
+			$json = json_decode($content, true);
+			if (!is_null($events['events'])) {
+			// Loop through each event
+				foreach ($events['events'] as $event) {
+					// Reply only when message sent is in 'text' format
+					if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+						// Get text sent
+						$text = $event['message']['text'];
+						// Get replyToken
+						$replyToken = $event['replyToken'];
+						$messages = [
+							['type' => 'text',
+							'text' => 'Username-Authentication'],
+							['type' => 'text',
+							'text' => $tt]
+						];
+
+						// Make a POST Request to Messaging API to reply to sender
+						$url = 'https://api.line.me/v2/bot/message/reply';
+						$data = [
+							'replyToken' => $replyToken,
+							'messages' => [$messages[1]],
+						];
+						$post = json_encode($data);
+						$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+						$ch = curl_init($url);
+						curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+						curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+						curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+						curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+						$result = curl_exec($ch);
+						curl_close($ch);
+					}
+				}
+			}
 
 			echo $result . "\r\n";
 		}
