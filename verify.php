c0719eb11c8bbfa4847f29e2735db7519e112066
needlesszero <blue.applez04@gmail.com>
2018-02-02 16:14:35 +0700

    /ๅ--

--- verify.php
+++ verify.php
@@ -1,10 +1,6 @@
 <?php
 $access_token = 'KygJBTnV/xAS9QNhJgQymbEZFw92G8Mj0RjrD3ycZEYbO9+I1a4e4dUqbvIo9Rv+OROLnYrlXO5peau/5MeriEs/kUu4iu0WojXBWLqXqj60DFs60UEbMhmV1fc5mEFF+GXDdqzqmAs+50FUkrVwCwdB04t89/1O/w1cDnyilFU=';
 
-$messages = [
-				['type' => 'text',
-				'text' => 'tt']
-			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
