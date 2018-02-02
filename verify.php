1b2481292cd604564c33a5aafecc9eb7dc1ead2f
needlesszero <blue.applez04@gmail.com>
2018-02-02 12:20:59 +0700

    123213

--- verify.php
+++ verify.php
@@ -9,8 +9,10 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
+$result[0] = curl_exec($ch);
+$result[1] = curl_exec($ch);
 curl_close($ch);
 
-//echo $result;
+echo $result[0];
+echo $result[1];
 ?>
