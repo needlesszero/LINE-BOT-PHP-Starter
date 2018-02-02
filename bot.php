f16373f0ae45f757412b7e83dd955ea99d8ba796
needlesszero <blue.applez04@gmail.com>
2018-02-02 12:07:30 +0700

    123123

--- bot.php
+++ bot.php
@@ -4,7 +4,7 @@
 $access_token = 'KygJBTnV/xAS9QNhJgQymbEZFw92G8Mj0RjrD3ycZEYbO9+I1a4e4dUqbvIo9Rv+OROLnYrlXO5peau/5MeriEs/kUu4iu0WojXBWLqXqj60DFs60UEbMhmV1fc5mEFF+GXDdqzqmAs+50FUkrVwCwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
-$content = file_get_contents('php://input');
+$content = file_get_contents('php://stdin');
 // Parse JSON
 $events = json_decode($content, true);
 
