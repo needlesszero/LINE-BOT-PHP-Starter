c404417c4031ad616002212da5da36d12bc47874
needlesszero <blue.applez04@gmail.com>
2018-02-07 13:19:25 +0700

    /ๅ-ๅ/-

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ $url = 'https://spreadsheets.google.com/feeds/list/1frT-QCU8A5Egh1XV3nW-8miICBvA
 $content = file_get_contents($url);
 $json = json_decode($content, true);
 $findPlace = false;
-$json = $json['feed']['entry'][0]['gsx$customername']['$t'];
+$json = $json['feed']['entry'];
 echo $json;
 
 if (!is_null($events['events'])) {
