8facc28b3808788ea291a3dc352bd06c70f84813
needlesszero <blue.applez04@gmail.com>
2018-03-08 11:19:24 +0700

    123213

--- bot.php
+++ bot.php
@@ -24,19 +24,10 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		$uid = $event['source']['userId'];
 
-		foreach ($jsonAuthen['feed']['entry'] as $key=>$value) {
-					if(stripos($jsonAuthen['feed']['entry'][$key]['gsx$userid']['$t'],$event['source']['userId']) !== false){
-						}
-					else{
-							$tt = 'Authentication Failed';
-							break;
-						}
+		foreach ($jsonAuthen['feed']['entry'] as $key=>$value) {					
+			if(stripos($jsonAuthen['feed']['entry'][$key]['gsx$userid']['$t'],$event['source']['userId']) !== false){
 
-					}
-
-		
-
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+				if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -231,6 +222,19 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
+				
+				}
+			else{
+					$tt = 'Authentication Failed';
+					break;
+				}
+			}
+
+					
+
+		
+
+		
 
 	}
 }
