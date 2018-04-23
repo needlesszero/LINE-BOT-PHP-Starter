eeb005d65be8dbdaa23cdadfe807d2bd3aaaac60
needlesszero <blue.applez04@gmail.com>
2018-04-24 01:23:25 +0700

    123

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$access_token = 'KygJBTnV/xAS9QNhJgQymbEZFw92G8Mj0RjrD3ycZEYbO9+I1a4e4dUqbvIo9Rv+OROLnYrlXO5peau/5MeriEs/kUu4iu0WojXBWLqXqj60DFs60UEbMhmV1fc5mEFF+GXDdqzqmAs+50FUkrVwCwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'am1ve2WFidjcmj3wS/c+QqoulSxJ4UKI0iwjrdoN5/HOcuFiORAgVGSQ/g3kqRZS5LxyKOklfZo+oQ+HWnn++keNv9IjRjZk2rA7GM1X9AKeYUui1G/XFcOXcD05EqDUkgsomTrvvui2K4hGb9DuzgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
