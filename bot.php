4e3bd8a0381cb189a380fa5aa38cc4a5edd84a45
needlesszero <blue.applez04@gmail.com>
2018-04-24 00:27:57 +0700

    123

--- bot.php
+++ bot.php
@@ -1,38 +1,25 @@
 <?php
-
-
 $access_token = 'KygJBTnV/xAS9QNhJgQymbEZFw92G8Mj0RjrD3ycZEYbO9+I1a4e4dUqbvIo9Rv+OROLnYrlXO5peau/5MeriEs/kUu4iu0WojXBWLqXqj60DFs60UEbMhmV1fc5mEFF+GXDdqzqmAs+50FUkrVwCwdB04t89/1O/w1cDnyilFU=';
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-
 $urlAuthen = 'https://spreadsheets.google.com/feeds/list/1frT-QCU8A5Egh1XV3nW-8miICBvA6xTTSRHWG26lyqE/3/public/values?alt=json';
 $contentAuthen = file_get_contents($urlAuthen);
 $jsonAuthen = json_decode($contentAuthen, true);
-
 $url = 'https://spreadsheets.google.com/feeds/list/1frT-QCU8A5Egh1XV3nW-8miICBvA6xTTSRHWG26lyqE/1/public/values?alt=json';
 $content = file_get_contents($url);
 $json = json_decode($content, true);
 $findPlace = false;
-if(strcmp("U237064aef8646f8a0f49e8794a05aa3f","U237064aef8646f8a0f49e8794a05aa3f") !== 0 ){
-	echo "failed";
-}
-else {
-	echo "success";
-}
-
+echo strcmp("สำนักงานบังคับคดีจังหวัดบุรีรัมย์","สำนักงานบังคับคดีจังหวัดบุรีรัมย์");
 $authen = false;
-
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		$uid = $event['source']['userId'];
-
 		foreach ($jsonAuthen['feed']['entry'] as $key=>$value) {					
-			if(strcmp($jsonAuthen['feed']['entry'][$key]['gsx$userid']['$t'],$event['source']['userId']) !== false){
+			if(stripos($jsonAuthen['feed']['entry'][$key]['gsx$userid']['$t'],$event['source']['userId']) !== false){
 				$authen = true;
 				break;
 				}
@@ -42,7 +29,6 @@ if (!is_null($events['events'])) {
 				}
 			}
 			
-
 			if($authen !== false) {
 				if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
@@ -50,8 +36,6 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			
-
-
 			if(preg_match('/^-help/', $event['message']['text'])||preg_match('/^-h/', $event['message']['text'])){
 				$tt = '-help : เพื่อแสดงคำสั่ง'."\n".
 					  '-f <คำค้นหา> : เพื่อค้นหาสถานที่ประกอบคำที่ต้องการค้นหา'."\n".
@@ -62,28 +46,52 @@ if (!is_null($events['events'])) {
 					  '-up / -ut <ชื่อหน่วยงาน> : แสดง UptimeDurations';
 					  
 			}
-
-			else if(preg_match('/^-uid/', $event['message']['text'])){
+			if(preg_match('/^-uid/', $event['message']['text'])){
 				$tt = $uid;
 					  
 			}
-
+			/*elseif(preg_match('/^-f/', $event['message']['text'])){
+							$tt ='สถานที่ที่ค้นหา :'."\n";
+							$c = 0;
+							foreach ($json['feed']['entry'] as $key=>$value){
+								$data = $event['message']['text'];    
+								$whatIWant = substr($data, strpos($data, ' ') + 1);
+								if(stripos($json['results'][$key]['Customer_Name'],$whatIWant) !== false){
+									$tt .= '-'.$json['results'][$key]['Customer_Name']."\n";
+									$c += 1;
+									$findPlace = true;					
+								}
+							}
+							$url = 'https://powerful-badlands-66623.herokuapp.com/im2.json';
+							$content = file_get_contents($url);
+							$json = json_decode($content, true);
+							foreach ($json['results'] as $key=>$value){
+								$data = $event['message']['text'];    
+								$whatIWant = substr($data, strpos($data, ' ') + 1);
+								if(stripos($json['results'][$key]['Customer_Name'],$whatIWant) !== false){
+									$tt .= '- '.$json['results'][$key]['Customer_Name']."\n";
+									$c += 1;
+									$findPlace = true;					
+								}
+							}
+							$tt .='ค้นพบทั้งหมด '.$c.' สถานที่' ;
+							if($c==0){
+								$tt='ไม่พบข้อมูล';
+							}
+						}*/
 			else{
 				foreach ($json['feed']['entry'] as $key=>$value) {
 						//if($event['message']['text'] == 'status'){
 					
-
 						if(stripos($json['feed']['entry'][$key]['gsx$customername']['$t'],$event['message']['text']) !== false){
 								$tt = 'ชื่อ: '."\n".$json['feed']['entry'][$key]['gsx$customername']['$t']."\n".'จังหวัด: '.$json['feed']['entry'][$key]['gsx$province']['$t']."\n".'Status: Down'.$json['status']['$t']."\n".'DowntimeDuration: '.$json['feed']['entry'][$key]['gsx$downtimedorations']['$t']."\n".'LastDownTimes: '.$json['feed']['entry'][$key]['gsx$lastdowntimes']['$t']."\n".'Customer_SLA: '.$json['feed']['entry'][$key]['gsx$customersla']['$t'];
 							$findPlace = true;
 							}
-
 													
 						if($json['feed']['entry'][$key]['gsx$customername']['$t'] === $event['message']['text']){
 							$tt = 'ชื่อ: '."\n".$json['feed']['entry'][$key]['gsx$customername']['$t']."\n".'จังหวัด: '.$json['feed']['entry'][$key]['gsx$province']['$t']."\n".'Status: Down'.$json['status']['$t']."\n".'DowntimeDuration: '.$json['feed']['entry'][$key]['gsx$downtimedorations']['$t']."\n".'LastDownTimes: '.$json['feed']['entry'][$key]['gsx$lastdowntimes']['$t']."\n".'Customer_SLA: '.$json['feed']['entry'][$key]['gsx$customersla']['$t'];
 							$findPlace = true;
 						}
-
 						elseif(preg_match('/^-stat/', $event['message']['text'])){
 							$data = $event['message']['text'];    
 							$whatIWant = substr($data, strpos($data, ' ') + 1);
@@ -94,7 +102,6 @@ if (!is_null($events['events'])) {
 							}
 							else $tt = 'ไม่พบข้อมูล';	
 						}
-
 						elseif(preg_match('/^-dt/', $event['message']['text']) || preg_match('/^-down/', $event['message']['text'])){
 							$data = $event['message']['text'];    
 							$whatIWant = substr($data, strpos($data, ' ') + 1);
@@ -105,7 +112,6 @@ if (!is_null($events['events'])) {
 							}
 							else $tt = 'ไม่พบข้อมูล';	
 						}
-
 						elseif(preg_match('/^-ltd/', $event['message']['text']) || preg_match('/^-lastd/', $event['message']['text'])){
 							$data = $event['message']['text'];    
 							$whatIWant = substr($data, strpos($data, ' ') + 1);
@@ -116,7 +122,6 @@ if (!is_null($events['events'])) {
 							}
 							else $tt = 'ไม่พบข้อมูล';	
 						}
-
 						else {
 							if($findPlace==false){
 								$tt = '-help เพื่อแสดงคำสั่ง';	
@@ -124,12 +129,10 @@ if (!is_null($events['events'])) {
 						}				
 					
 				}
-
 				if($findPlace==false){				
 					$url = 'https://spreadsheets.google.com/feeds/list/1frT-QCU8A5Egh1XV3nW-8miICBvA6xTTSRHWG26lyqE/2/public/values?alt=json';
 					$content = file_get_contents($url);
 					$json = json_decode($content, true);
-
 					foreach ($json['results'] as $key=>$value) {
 							//if($event['message']['text'] == 'status'){
 							if(stripos($json['results'][$key]['Customer_Name'],$event['message']['text']) !== false){
@@ -157,7 +160,6 @@ if (!is_null($events['events'])) {
 							}
 							else $tt = 'ไม่พบข้อมูล';	
 						}
-
 						elseif(preg_match('/^-ltd/', $event['message']['text']) || preg_match('/^-lastd/', $event['message']['text'])){
 							$data = $event['message']['text'];    
 							$whatIWant = substr($data, strpos($data, ' ') + 1);
@@ -169,21 +171,15 @@ if (!is_null($events['events'])) {
 							else $tt = 'ไม่พบข้อมูล';	
 						}
 						else $tt = '-help เพื่อแสดงคำสั่ง';		
-
 						$command = strtok($text, ' ');
 									
 						
 					}
-
 				}
 			}
 			};
-
-
 			
-
 			
-
 			// Build message to reply back
 			$messages = [
 				['type' => 'text',
@@ -191,7 +187,6 @@ if (!is_null($events['events'])) {
 				['type' => 'text',
 				'text' => $tt]
 			];
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -200,7 +195,6 @@ if (!is_null($events['events'])) {
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -209,10 +203,8 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
 			echo $result . "\r\n";
 		}
-
 	}
 }
 echo "OK";
