1f2f3b50124df21dfe0b63e1d693d7ac9ad981ee
needlesszero <blue.applez04@gmail.com>
2018-05-03 04:42:24 +0700

    123

--- botback_final.php
+++ botback_final.php
@@ -338,4 +338,15 @@ function call_node(){
 	// close cURL resource, and free up system resources
 	curl_close($ch);
 
+	$ch = curl_init();
+	// set URL and other appropriate options
+	curl_setopt($ch, CURLOPT_URL, "https://line.gin.totisp.net/call_nodeup.php");
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+
+	// grab URL and pass it to the browser
+	curl_exec($ch);
+
+	// close cURL resource, and free up system resources
+	curl_close($ch);
+	
 }
